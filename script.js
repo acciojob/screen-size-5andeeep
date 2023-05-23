43bb6557086bcbb95db20db7d883bfcaa34d3b7f
Sandeep <newsandeep03@gmail.com>
2023-05-23 14:22:26 +0530

    ans added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+const screenSize = document.createElement("h1");
+screenSize.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+
+document.body.append(screenSize);
+
+// onresize is a property that takes a function value. which is why
+// we write window.onresize = function(){}... here function is value
+
+window.onresize = function(){
+    screenSize.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+}
